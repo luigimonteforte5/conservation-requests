6dab05572a11980dcf39b6e6986782e0f9baa911
Luigi Monteforte <monteforteluigi28@gmail.com>
2026-07-14 19:20:41 +0200

    Initial Commit

---
+++ src/main/java/com/luigimonteforte/conservationrequests/ConservationRequestsApplication.java
@@ -0,0 +1,13 @@
+package com.luigimonteforte.conservationrequests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConservationRequestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConservationRequestsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luigimonteforte/conservationrequests/ConservationRequestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luigimonteforte.conservationrequests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConservationRequestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
